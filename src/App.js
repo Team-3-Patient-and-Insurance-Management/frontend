222c9de7238d858469fc2c91f03304076a817d67
Sasha <svelet@iu.edu>
2024-03-08 17:42:53 -0500

    pushing whatever to my branch

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import LogIn from "./pages/LogIn/LogIn";
 import SignUp from "./pages/SignUp/SignUp";
+import { AuthProvider } from "./context/AuthContext";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import UpdateProfile from "./pages/UpdateProfile/UpdateProfile";
+import PrivateRoute from "./Components/PrivateRoute";
 import DoctorProfile from "./pages/Profile/DoctorProfile";
 import DoctorUser from "./data/Doctor/DoctorUser";
 
 export default function App() {
     return (
         <Router>
+            <AuthProvider>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="login" element={<LogIn />} />
                 <Route path="signup" element={<SignUp />} />
                 <Route path="doctor-profile" element={<DoctorProfile />} />
-                <Route path="doctorUser" element={<DoctorUser />} />å
-            </Routes>
+                <Route path="doctorUser" element={<DoctorUser />} />
+
+                <Route path="forgot-password" element={<ForgotPassword />} />
+                    
+                    {/* Define a route with PrivateRoute as the element */}
+                    <Route element={<PrivateRoute />}>
+                        {/* Define protected routes as children */}
+                        <Route path="userprofile" element={<UpdateProfile />} />
+                        <Route path="dashboard" element={<Dashboard />} />
+                    </Route>
+                 </Routes>
+            </AuthProvider>
         </Router>
     );
 }
